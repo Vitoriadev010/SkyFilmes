1ae8b5b8cd599348f0380bc9079ed2f892cdd173
AnaLivyaDev <neg.livyacrida@gmail.com>
2025-10-30 13:38:20 -0300

    .

---
+++ service/token.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+const SECRET = 'APIbilheteria';
+
+
+function autenticarToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    console.log(authHeader);
+
+    if (!authHeader) return res.status(401).json({ erro: 'token não enviado' });
+
+    const token = authHeader.startsWith('Bearer ') ? authHeader.split(' ')[1] : authHeader;
+
+    jwt.verify(token, SECRET, (err, decoded) => {
+        if (err) {
+            console.log(err);
+            return res.status(403).json({ erro: 'token inválido ou expirado' });
+        }
+        req.cliente = decoded;
+        next()
+    });
+}
+
+module.exports = { autenticarToken }; //para usar em outros arquivos
