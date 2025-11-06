2f73ccd521066fe00604ea891e1d449c1b9f736a
AnaLivyaDev <neg.livyacrida@gmail.com>
2025-11-06 13:57:55 -0300

    -livya

--- controllers/sessoesController.js
+++ controllers/sessoesController.js
@@ -144,7 +144,7 @@ exports.criarSessao = async (req, res) => {
 
 exports.editarSessao = async (req, res) => {
     const authHeader = req.headers.authorization;
-    const { idSessao, idFilme, idSala, hora, data } = req.body;
+    const { idSessao, idFilme, idSala, hora, data, status } = req.body;
 
     try {
         const token = authHeader.startsWith('Bearer ') ? authHeader.split(' ')[1] : authHeader;
@@ -174,6 +174,12 @@ exports.editarSessao = async (req, res) => {
         if (idSala !== undefined) sessao.idSala = idSala;
         if (hora !== undefined) sessao.hora = hora;
         if (data !== undefined) sessao.data = data;
+        if (status !== undefined) sessao.status = status;
+
+        if (status !== undefined && ![0, 1].includes(status)) {
+            return res.status(400).json({ erro: 'Status inválido. Use 0 para inativo ou 1 para ativo.' });
+        }
+
 
         await sessao.save();
 
@@ -194,50 +200,12 @@ exports.editarSessao = async (req, res) => {
 };
 
 
-exports.deletarSessao = async (req, res) => {
-    console.lof('deletando sessao');
-    const authHeader = req.headers.authorization;
-    const { idSessao } = req.body;
-
-    try {
-        const token = authHeader.startsWith('Bearer ') ? authHeader.split(' ')[1] : authHeader;
-
-
-        const autenticado = jwt.verify(token, SECRET, (err, decoded) => {
-            if (err) {
-                console.log(err);
-                return res.status(403).json({ erro: 'token inválido ou expirado' });
-            }
-
-            console.log(decoded);
-            return decoded;
-        });
-
-        console.log('gestor:', autenticado);
-
-        const sessao = await models.sessoes.findOne({
-            where: {
-                idSessao: idSessao
-            }
-        });
-
-        if (!sessao) {
-            return res.status(404).json({ erro: 'Sessão não encontrada para o id informado.' });
-        }
-
-        await sessao.destroy();
-
-        res.status(200).json({ mensagem: 'Sessão deletada com sucesso.' });
-
-    } catch (error) {
-        console.error('Erro ao deletar sessão:', error);
-    }
-};
 
 
 exports.listarSessoes = async (req, res) => {
     const authHeader = req.headers.authorization;
-
+    const { status } = req.body;
+    // listar sessões por filtro de status, ex: ativo/inativo e ativo e inativo - fazer depois
 
     try {
         const token = authHeader.startsWith('Bearer ') ? authHeader.split(' ')[1] : authHeader;
@@ -255,8 +223,19 @@ exports.listarSessoes = async (req, res) => {
 
         console.log('gestor:', autenticado);
 
-        const listaSessoes = await sessoes.findAll({
-            attributes: ['idSessao', 'idFilme', 'idSala', 'hora', 'data']
+        // filtro por status se fornecido no corpo da requisição
+        // const where = {};
+
+        if (status !== undefined) {
+            if (Array.isArray(status)) {
+                where.status = { [Op.in]: status };
+            } else {
+                where.status = status;
+            }
+        }
+        const listaSessoes = await models.sessoes.findAll({
+            attributes: ['idSessao', 'idFilme', 'idSala', 'hora', 'data', 'status'],
+            where
         });
 
         res.status(200).json(listaSessoes);
@@ -269,6 +248,7 @@ exports.listarSessoes = async (req, res) => {
 // lista sessoes futuras para clientes
 exports.listarSessoesFuturas = async (req, res) => {
     const authHeader = req.headers.authorization;
+    // listar sessões por filtro de status, apenas ativos(futuras)
 
     try {
         const token = authHeader.startsWith('Bearer ') ? authHeader.split(' ')[1] : authHeader;
@@ -286,7 +266,6 @@ exports.listarSessoesFuturas = async (req, res) => {
 
         console.log('cliente:', autenticado);
 
-        const agora = new Date();
 
         const listaSessoesFuturas = await sessoes.findAll({
             where: {
