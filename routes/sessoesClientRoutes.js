c7a3fa79c40d56ebbb14fbe3014df591b3f15613
Vitoriadev010 <mariavitoria11122007@exemplo.com>
2025-11-18 14:42:57 -0300

    .

--- routes/sessoesClientRoutes.js
+++ routes/sessoesClientRoutes.js
@@ -9,10 +9,12 @@ const sessoesController = require("../controllers/sessoesController");
 const { autenticarTokenCliente, autenticarTokenGestor } = require("../service/token");
 
 
+
 // cliente 
-router.get("/detalharSessao", autenticarTokenCliente, sessoesController.detalhesSessao);
-router.get("/sessoesPorFilme/:idFilme", autenticarTokenCliente, sessoesController.sessoesPorFilme);
-router.get("/sessaoativa", autenticarTokenCliente, sessoesController.sessaoAtivo);
+router.get("/detalharSessao",  sessoesController.detalhesSessao);
+router.get("/sessoesPorFilme/:idFilme", sessoesController.sessoesPorFilme);
+router.get("/sessaoativa", sessoesController.sessaoAtivo);
+
 
 
 
